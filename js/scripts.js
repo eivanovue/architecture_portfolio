f883fad49d5b75e4d493e8e312492ad378443a5a
Emil Ivanov <emil@eivanovue.com>
2020-06-09 15:01:06 +0100

    A lot of changes to layout and design of architecture portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -36,10 +36,7 @@
     });
   
     // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
+    
   
     // Collapse Navbar
     var navbarCollapse = function() {
